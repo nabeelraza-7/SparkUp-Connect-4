e3d1437a492bef52d9f060c725a0330818124556
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-07-27 02:00:03 +0500

    Update Connect4.py

--- Connect4.py
+++ Connect4.py
@@ -1,5 +1,29 @@
 import numpy as np
 import pandas as pd
 
-Board = np.zeros((8, 7))
-print(Board)
+class Connect_4:
+    Turns = 0
+
+    def __init__(self, size = (7, 8)):
+        self.board = np.zeros(size)
+        print(self.board)
+    
+    def show(self):
+        print(self.board)
+
+    def P1Turn(self, pos):
+        Connect_4.Turns += 1
+        self.board[pos[0], pos[1]] = 1
+
+    def p2Turn(self, pos):
+        self.board[pos[0], pos[1]] = 2
+
+    def WinCheck(self):
+        if Connect_4.Turns>3:
+            for i in range(self.board.shape[0]):
+                for j in range(2, self.board.shape[1]-2):
+                    if self.board[i][j] == key:
+                        pass
+            pass
+        pass
+        # return None
